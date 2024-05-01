8ad4b38bdb0afb1de02af8e47dd331d2f187718e
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2024-04-30 21:26:08 -0400

    Update compile.sh to latest version

--- compile.sh
+++ compile.sh
@@ -24,7 +24,7 @@ echo "Syncing skia dependencies"
 python3 tools/git-sync-deps
 
 echo "Compiling skia"
-gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false"
+gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_z>
 ninja -C out/Release-x64 skia modules
 
 echo "Download Aseprite and compile"
@@ -32,7 +32,7 @@ cd /output
 
 if [ ! -d "/output/aseprite" ]
 then
-  git clone -b v1.2.40 --recursive https://github.com/aseprite/aseprite.git
+  git clone -b v1.3.6 --recursive https://github.com/aseprite/aseprite.git
 fi
 
 cd aseprite
